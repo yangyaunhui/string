c0c2ad4ce344b3400d411c7060e78812621318dd
@yang-yuanhui <yang07061237@163.com>
2021-07-16 08:40:17 +0800

    1.作业

--- src/com/hp/Test/Test04.java
+++ src/com/hp/Test/Test04.java
@@ -15,6 +15,7 @@ public class Test04 {
     public static void main(String[] args) {
         //创建集合,添加元素0-9
         ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
         list.add(1);
         list.add(2);
         list.add(3);
@@ -25,7 +26,7 @@ public class Test04 {
         list.add(8);
         list.add(9);
         //获取子集[3,4,5,6]
-        List<Integer> integers = list.subList(2, 6);
+        List<Integer> integers = list.subList(3, 7);
         System.out.println("integers = " + integers);
         for (int i = 0; i < integers.size(); i++) {
             int i1 = integers.get(i);
@@ -37,6 +38,7 @@ public class Test04 {
         //输出原集合
         System.out.println(list);
         //删除集合中的[7,8,9]
-
+        list.subList(7,10).clear();
+        System.out.println("list = " + list);
     }
 }
